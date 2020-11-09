de57ee2843f38d90fb0b98260ce7045de3bf94a3
KyPT2503 <62481511+KyPT2503@users.noreply.github.com>
2020-11-09 11:34:19 +0700

    bai tap

---
+++ script.js
@@ -0,0 +1,25 @@
+function useAlert()
+{
+    alert("Hi teacher !");
+}
+
+function useConfirm()
+{
+    confirm("Hi teacher !");
+}
+
+function useGetElement()
+{
+    document.getElementById("demo01").innerHTML="Hi teacher !";
+}
+
+function useDocumentWrite()
+{
+    document.write("Hi teacher !");
+
+}
+
+function useConsole()
+{
+    console.log('hi teacher !');
+}
